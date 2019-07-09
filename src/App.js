79a10f7313de465aeb5de6c58e302f1712b4c388
Anzen Digital <alex.c.valera94@hotmail.com>
2019-07-09 17:12:25 -0500

    Last changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,58 @@
 import React, { useState, useEffect, Fragment } from 'react';
+import Form from './components/Form';
+import axios from 'axios';
+import Song from './components/Song';
+import Info from './components/Info';
 
 function App(props) {
-    return <div>Hola</div>;
+    // Utilizar useState con 3 states diferentes
+    const [artist, addArtist] = useState('');
+    const [lyrics, addLyrics] = useState([]);
+    const [info, addInfo] = useState({});
+
+    // Metodo para consultar la API de letras de canciones
+    const fetchAPILyrics = async search => {
+        const { artist, song } = search;
+        const URL = `https://api.lyrics.ovh/v1/${artist}/${song}`;
+
+        // consultar la api
+        const result = await axios(URL);
+
+        // Almacenar el artista quw se buscó
+        addArtist(artist);
+
+        // Almacenar la letra en el state
+        addLyrics(result.data.lyrics);
+    };
+
+    // Metod para consultar la API de información
+    const consultarAPIArtisInfo = async () => {
+        if (artist) {
+            const URL = `https://theaudiodb.com/api/v1/json/1/search.php?s=${artist}`;
+
+            const result = await axios(URL);
+            addInfo(result.data.artists[0]);
+        }
+    };
+
+    useEffect(() => {
+        consultarAPIArtisInfo();
+    }, [artist]);
+
+    return (
+        <Fragment>
+            <Form fetchAPILyrics={fetchAPILyrics} />
+
+            <div className="row">
+                <div className="col-md-6">
+                    <Info info={info} />
+                </div>
+                <div className="col-md-6">
+                    <Song lyrics={lyrics} />
+                </div>
+            </div>
+        </Fragment>
+    );
 }
 
 export default App;
---
+++ src/components/Form.js
@@ -0,0 +1,69 @@
+import React, { useState } from 'react';
+
+function Form({ fetchAPILyrics }) {
+    const [search, addSearch] = useState({
+        artist: '',
+        song: ''
+    });
+
+    // Funcion para actualizar el state de los inputs
+    const updateState = e => {
+        addSearch({
+            ...search,
+            [e.target.name]: e.target.value
+        });
+    };
+
+    // Cuando se hace submit al Form
+    const sendData = e => {
+        e.preventDefault();
+        fetchAPILyrics(search);
+    };
+
+    return (
+        <div className="bg-info">
+            <div className="container">
+                <div className="row">
+                    <form onSubmit={sendData} className="col card text-white bg-transparent  mb-5 pt-5 pb-2">
+                        <fieldset>
+                            <legend className="text-center">Buscador Letras Canciones</legend>
+                            <div className="row">
+                                <div className="col-md-6">
+                                    <div className="form-group">
+                                        <label>Artista</label>
+                                        <input
+                                            type="text"
+                                            className="form-control"
+                                            name="artist"
+                                            placeholder="Nombre Artista"
+                                            onChange={updateState}
+                                            required
+                                        />
+                                    </div>
+                                </div>
+                                <div className="col-md-6">
+                                    <div className="form-group">
+                                        <label>Canción</label>
+                                        <input
+                                            type="text"
+                                            className="form-control"
+                                            name="song"
+                                            placeholder="Nombre Canción"
+                                            onChange={updateState}
+                                            required
+                                        />
+                                    </div>
+                                </div>
+                            </div>
+                            <button type="submit" className="btn btn-primary float-right">
+                                Buscar
+                            </button>
+                        </fieldset>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Form;
